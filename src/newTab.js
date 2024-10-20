fd6f1f6a54c0f6f7857f5471f8e53e88a8a8efca
laktam <laktamlaktamabdellah@gmail.com>
2024-10-20 12:21:18 +0100

    add draggable method to make  html element draggable

--- src/newTab.js
+++ src/newTab.js
@@ -1,6 +1,9 @@
 import { clear, find, ls, noCommandFound, printToConsole, pwd } from "./actions/domActions.js";
 
 let terminal = document.getElementById('terminal');
+draggable(terminal)
+
+
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.action == "ls") {
@@ -31,3 +34,42 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   sendResponse();
   return true;
 });
+
+
+function draggable(element){
+  let mouseClickX = 0
+  let mouseClickY = 0
+  let x = Number(element.style.left.slice(0,-2))
+  let y = Number(element.style.top.slice(0,-2))
+  let drag = false;
+
+  element.addEventListener("mousedown", (e)=>{
+    drag = true
+    mouseClickX = e.clientX
+    mouseClickY = e.clientY
+    x = Number(element.style.left.slice(0,-2))
+    y = Number(element.style.top.slice(0,-2))
+    console.log("mouse down")
+  })
+
+  document.addEventListener("mouseup", (e)=>{
+    drag = false;
+    console.log("mouse up")
+    mouseClickX = 0
+    mouseClickY = 0
+  })
+
+  element.addEventListener("mousemove",(e)=>{
+    if(drag){
+      let currentMouseX = e.clientX
+      let currentMouseY = e.clientY
+    
+      
+      console.log("x y", x, y)
+      element.style.left = x + currentMouseX - mouseClickX + "px"
+      element.style.top = y +  currentMouseY - mouseClickY + "px"
+      
+      console.log("left and top", element.style.left, element.style.top)
+    }
+  })
+}
