bacb2745d09a2355f23aff99f9c86a1818f41a2c
laktam <laktamlaktamabdellah@gmail.com>
2024-10-21 19:57:13 +0100

    close terminal when close button is clicked

--- build/new-tab/script.js
+++ build/new-tab/script.js
@@ -1,5 +1,10 @@
 const input = document.getElementById('commandInput')
 const terminal = document.getElementById('terminal')
+const closeButton = document.getElementById('close-button')
+
+closeButton.addEventListener('click', ()=>{
+  document.getElementById('terminal-container').remove()
+})
 
 terminal.addEventListener("click", () => {
     input.focus();
