b290b49b84536305a48fc269b32e61b6cb530ea3
laktam <laktamlaktamabdellah@gmail.com>
2024-10-22 21:42:50 +0100

    add consts file for help html content

---
+++ src/actions/constants.js
@@ -0,0 +1,28 @@
+export const fullHelp = `<div>
+For more information on a specific command, type HELP command-name
+<br><br>
+<div id='help-container'>
+<div id='help-commands'>
+    <div>cd</div>
+    <div>ls</div>
+    <div>create</div>
+    <div>group</div>
+    <div>ungroup</div>
+    <div>rm</div>
+    <div>clear</div>
+    <div>find</div>
+</div>
+
+<div id='help-text'>
+    <div>move to a specific tab (use ls for tab indexes)</div>
+    <div>print open tabs with there index</div>
+    <div>create a new tab with a given url</div>
+    <div>create a new group or add a tab to existing group</div>
+    <div>ungroup a group or remove a tab from a group</div>
+    <div>close a tab or group tabs</div>
+    <div>clear terminal</div>
+    <div>search for tabs with a specific keyword</div>
+</div>
+</div>
+</div>
+`
