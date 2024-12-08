db587bbbf9b9132fc12329062fb9b90bbf8f1b8d
laktam <laktamlaktamabdellah@gmail.com>
2024-12-08 17:43:57 +0100

    create css and html in js to use in content script to inject terminal in the open pages

---
+++ src/terminal-css-html/terminal-html.js
@@ -0,0 +1,27 @@
+export const terminalContainer = document.createElement('div');
+terminalContainer.id = "terminal-container";
+
+terminalContainer.innerHTML = `
+  <div id="terminal-header">
+    <div id="start">
+      Command Prompt
+    </div>
+    <div id="end">
+      <span class="terminal-header-button" id="minimize-button">_</span>
+      <span class="terminal-header-button" id="maximize-button">□</span>
+      <span class="terminal-header-button" id="close-button">X</span>
+    </div>
+  </div>
+  <div id="terminal">
+    <div class="output" id="output">
+      Browser CLI
+      <br>
+      Type help to view command list or help {command} for specific command syntax.
+      <br><br>
+    </div>
+    <div class="terminal-input">
+      <span class="prompt">></span>
+      <input type="text" id="commandInput" autocomplete="off">
+    </div>
+  </div>
+`;
