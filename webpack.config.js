cdb92367cc05af40395f705820f0fc27c2053fd9
laktam <laktamlaktamabdellah@gmail.com>
2024-12-08 17:45:42 +0100

    update webpack config to use content script not new tab script

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,18 @@ const path = require("path");
 module.exports = {
   mode: 'production',
   entry: {
-    newTab: "./src/newTab.js",
+    // newTab: "./src/newTab.js",
     background: "./src/background.js",
+    content: "./src/content.js"
   },
   output: {
     filename: (pathData) => {
       if (pathData.chunk.name === 'newTab') {
         return 'new-tab/[name].js';
       }
-      if (pathData.chunk.name === 'background') {
-        return '[name].js';
-      }
+      // if (pathData.chunk.name === 'background') {
+      //   return '[name].js';
+      // }
       return '[name].js';
     },
     path: path.resolve(__dirname, "build"),
